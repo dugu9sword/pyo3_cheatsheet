1afc9979fb36c02505cf93c0ebd76d24e3ad3df7
zhouyi <dugu9sword@163.com>
2021-05-20 02:38:30 +0800

    some modification

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+use std::collections::HashMap;
+
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 mod bert_tokenization;
 
+/// add(a, b, /)
+/// --
+///
+/// This function compute range sum between two integers.
 #[pyfunction]
 fn range_sum(a: usize, b: usize) -> PyResult<usize> {
     let mut number = a;
@@ -31,6 +37,33 @@ fn bert_tokenize(sent: &str) -> PyResult<String> {
     Ok(bert_tokenization::bert_tokenize(sent)?)
 }
 
+#[pyfunction]
+#[text_signature = "(vec1, vec2, /)"]
+fn multiply(vec1: Vec<f32>, vec2: Vec<f32>) -> PyResult<Vec<f32>>{
+    let mut ret: Vec<f32> = Vec::with_capacity(vec1.len());
+    for i in 0..vec1.len(){
+        ret.push(vec1[i] * vec2[i]);
+    }
+    return Ok(ret);
+}
+
+#[pyfunction]
+fn merge_count(
+    count1: HashMap<String, i32>, 
+    count2: HashMap<String, i32>
+) -> PyResult<HashMap<String, i32>>{
+    let mut ret = count1.clone();
+    for (k, v) in count2.into_iter(){
+        if !ret.contains_key(&k){
+            ret.insert(k, v);
+        }else{
+            let to_add = *(ret.get(&k).unwrap());
+            ret.insert(k, v + to_add);
+        }
+    }
+    return Ok(ret);
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyo3_cheatsheet(py: Python, m: &PyModule) -> PyResult<()> {
@@ -38,6 +71,9 @@ fn pyo3_cheatsheet(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(range_sum_recursive, m)?)?;
 
     m.add_function(wrap_pyfunction!(bert_tokenize, m)?)?;
+    m.add_function(wrap_pyfunction!(multiply, m)?)?;
+    m.add_function(wrap_pyfunction!(merge_count, m)?)?;
+
 
     Ok(())
 }
--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-from pyo3_cheatsheet import range_sum, bert_tokenize, range_sum_recursive
+from pyo3_cheatsheet import range_sum, bert_tokenize, range_sum_recursive, multiply, merge_count
 import random
 import time
-from transformers import BertTokenizer, BertTokenizerFast
 
-tkzr = BertTokenizer.from_pretrained("bert-base-uncased")
-tkzr_fast = BertTokenizerFast.from_pretrained("bert-base-uncased")
+# from transformers import BertTokenizer, BertTokenizerFast
+
+# tkzr = BertTokenizer.from_pretrained("bert-base-uncased")
+# tkzr_fast = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
 
 def benchmark(func, args):
-    print(func(*args))
+    # print(func(*args))
     t1 = time.time()
     for i in range(100):
         func(*args)
@@ -20,6 +21,12 @@ def py_range_sum_recursive(a, b):
         return 0
     else:
         return a + py_range_sum_recursive(a + 1, b) 
+
+def py_multiply(a, b):
+    c = []
+    for ea, eb in zip(a, b):
+        c.append(ea * eb)
+    return c
 
 def tokenize(sent):
     return " ".join(tkzr.tokenize(sent))
@@ -33,7 +40,16 @@ def tokenize_fast(sent):
 # benchmark(py_range_sum_recursive, (1, 900, ))
 
 # benchmark(range_sum, (1, 900, ))
-sent = """the german army is continuing to lose weapons and ammunition, often without a trace. since the beginning of 2014, the armed forces has registered a loss of 39 weapons, 39 weapon components, and almost 20,000 rounds of ammunition. this is revealed in a classified listing of the german ministry of defense on which the redaktionsnetzwerk deutschland (rnd) ("editor network germany") has reported."""
-benchmark(tokenize, (sent, ))
-benchmark(tokenize_fast, (sent, ))
-benchmark(bert_tokenize, (sent, ))
+# sent = """the german army is continuing to lose weapons and ammunition, often without a trace. since the beginning of 2014, the armed forces has registered a loss of 39 weapons, 39 weapon components, and almost 20,000 rounds of ammunition. this is revealed in a classified listing of the german ministry of defense on which the redaktionsnetzwerk deutschland (rnd) ("editor network germany") has reported."""
+# benchmark(tokenize, (sent, ))
+# benchmark(tokenize_fast, (sent, ))
+# benchmark(bert_tokenize, (sent, ))
+
+
+x = [random.random()] * 10000
+y = [random.random()] * 10000
+benchmark(multiply, (x, y))
+benchmark(py_multiply, (x, y))
+
+# print(distance([1, 2, 3], [3, 4, 5]))
+# print(merge_count({"a": 1, "b": 3}, {"b": 2}))
